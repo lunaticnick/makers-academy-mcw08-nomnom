05486a3793892f426dee57954ae8918636ac86a2
kkumshayev <kumshkesh@gmail.com>
2018-01-17 15:21:25 +0000

    cannot create restaurants without names or descriptions

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,8 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true,
+                   length: { minimum: 3 }
+  validates :description, presence: true,
+                          length: { minimum: 10 }
 end
