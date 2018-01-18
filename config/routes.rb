4d36b0027910d0c72287ed1ac79ae7c593455660
Calum-W <cal7907@hotmail.co.uk>
2018-01-18 11:42:35 +0000

    Adds restaurants#index as a route. Users now working

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
-  # root 'restaurants#index'
+  root 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
