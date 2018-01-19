bf71a440db9c343798f1b615db1d5aca7f6eaa77
kkumshayev <kumshkesh@gmail.com>
2018-01-19 13:01:33 +0000

    average star rating working

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,7 @@ class Restaurant < ApplicationRecord
   belongs_to :user, required: false
   validates_presence_of :name, :description
   validates_length_of :description, :maximum => 500
+  def average_rating
+      reviews.length > 0 ? (20*(reviews.map{|review| review.rating }.sum.to_f) / reviews.length).round(2) : 0
+  end
 end
