e8a82a7b7fad07513702e0e9bbcebce785945659
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2018-01-19 10:48:57 +0000

    css

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :restaurants do
     resources :reviews
   end
-
+  get 'activity/mine'
+  get 'activity/feed'
+  #root to: 'activity#mine'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
